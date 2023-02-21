fa673a509473ad593d023304a766c1b4aad37588
Deniz İnan Şimşek <denizinansimsek@outlook.com>
2023-02-21 08:54:25 +0300

    clean code

--- CSharpTutorial.Classes.Core/Domains/Students/Student.cs
+++ CSharpTutorial.Classes.Core/Domains/Students/Student.cs
@@ -1,9 +1,4 @@
 ﻿using CSharpTutorial.Classes.Core.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpTutorial.Classes.Core.Domains.Students
 {
--- CSharpTutorial.Classes.Core/Helpers/SpeedHelper.cs
+++ CSharpTutorial.Classes.Core/Helpers/SpeedHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpTutorial.Classes.Core.Helpers
+﻿namespace CSharpTutorial.Classes.Core.Helpers
 {
     public class SpeedHelper
     {
